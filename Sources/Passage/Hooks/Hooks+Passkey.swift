ba90c3e62b8273aa0544fab8134592f6c1374a60
Max Rozdobudko <max.rozdobudko@gmail.com>
2026-05-03 10:57:01 +0300

    feat(hooks): Passkey hooks

--- Sources/Passage/Hooks/Hooks+Passkey.swift
+++ Sources/Passage/Hooks/Hooks+Passkey.swift
@@ -2,155 +2,49 @@ public import Vapor
 
 public extension Passage.Hooks {
 
-    protocol Account: Sendable {
+    protocol Passkey: Sendable {
 
-        // MARK: Registration Hooks
+        // MARK: Guest Registration Hooks
 
-        func willRegister(
-            with form: any RegisterForm,
+        func willBeginGuestRegistration(
+            with form: any PasskeySignupForm,
             on request: Request,
         ) async throws
-
-        func didRegister(
-            user: any User,
-            on request: Request,
-        ) async
-
-        // MARK: Login Hooks
-
-        func willLogin(
-            user: any User,
-            on request: Request,
-        ) async throws
-
-        func didLogin(
-            user: any User,
-            on request: Request,
-        ) async
-
-        // MARK: Logout Hooks
-
-        func willLogout(
-            user: any User,
-            on request: Request,
-        ) async throws
-
-        func didLogout(
-            user: any User,
-            on request: Request,
-        ) async
     }
 
 }
 
 // MARK: - Default Implementation
 
-public extension Passage.Hooks.Account {
+public extension Passage.Hooks.Passkey {
 
-    func willRegister(
-        with form: any RegisterForm,
+    func willBeginGuestRegistration(
+        with form: any PasskeySignupForm,
         on request: Request,
     ) async throws {}
-
-    func didRegister(
-        user: any User,
-        on request: Request,
-    ) async {}
-
-    func willLogin(
-        user: any User,
-        on request: Request,
-    ) async throws {}
-
-    func didLogin(
-        user: any User,
-        on request: Request,
-    ) async {}
-
-    func willLogout(
-        user: any User,
-        on request: Request,
-    ) async throws {}
-
-    func didLogout(
-        user: any User,
-        on request: Request,
-    ) async {}
 }
 
 // MARK: - Closure-Based Hooks
 
-public struct _AccountHooksClosures: Passage.Hooks.Account {
+public struct _PasskeyHooksClosures: Passage.Hooks.Passkey {
 
-    let _willRegister: (@Sendable (any RegisterForm, Request) async throws -> Void)?
-    let _didRegister: (@Sendable (any User, Request) async -> Void)?
+    let _willBeginGuestRegistration: (@Sendable (any PasskeySignupForm, Request) async throws -> Void)?
 
-    let _willLogin: (@Sendable (any User, Request) async throws -> Void)?
-    let _didLogin: (@Sendable (any User, Request) async -> Void)?
-
-    let _willLogout: (@Sendable (any User, Request) async throws -> Void)?
-    let _didLogout: (@Sendable (any User, Request) async -> Void)?
-
-    public func willRegister(
-        with form: any RegisterForm,
+    public func willBeginGuestRegistration(
+        with form: any PasskeySignupForm,
         on request: Request,
     ) async throws {
-        try await _willRegister?(form, request)
-    }
-
-    public func didRegister(
-        user: any User,
-        on request: Request,
-    ) async {
-        await _didRegister?(user, request)
-    }
-    
-    public func willLogin(
-        user: any User,
-        on request: Request,
-    ) async throws {
-        try await _willLogin?(user, request)
-    }
-
-    public func didLogin(
-        user: any User,
-        on request: Request,
-    ) async {
-        await _didLogin?(user, request)
-    }
-
-    public func willLogout(
-        user: any User,
-        on request: Request,
-    ) async throws {
-        try await _willLogout?(user, request)
-    }
-
-    public func didLogout(
-        user: any User,
-        on request: Request,
-    ) async {
-        await _didLogout?(user, request)
+        try await _willBeginGuestRegistration?(form, request)
     }
 }
 
-public extension Passage.Hooks.Account where Self == _AccountHooksClosures {
+public extension Passage.Hooks.Passkey where Self == _PasskeyHooksClosures {
 
     static func hook(
-        willRegisterUser : (@Sendable (any RegisterForm, Request) async throws -> Void)? = nil,
-        didRegisterUser  : (@Sendable (any User, Request) async -> Void)? = nil,
-        willLoginUser    : (@Sendable (any User, Request) async throws -> Void)? = nil,
-        didLoginUser     : (@Sendable (any User, Request) async -> Void)? = nil,
-        willLogoutUser   : (@Sendable (any User, Request) async throws -> Void)? = nil,
-        didLogoutUser    : (@Sendable (any User, Request) async -> Void)? = nil,
-    ) -> some Passage.Hooks.Account {
-        _AccountHooksClosures(
-            _willRegister: willRegisterUser,
-            _didRegister: didRegisterUser,
-            _willLogin: willLoginUser,
-            _didLogin: didLoginUser,
-            _willLogout: willLogoutUser,
-            _didLogout: didLogoutUser,
+        willBeginGuestRegistration: (@Sendable (any PasskeySignupForm, Request) async throws -> Void)? = nil,
+    ) -> some Passage.Hooks.Passkey {
+        _PasskeyHooksClosures(
+            _willBeginGuestRegistration: willBeginGuestRegistration
         )
     }
 
--- Sources/Passage/Hooks/Passage+Hooks.swift
+++ Sources/Passage/Hooks/Passage+Hooks.swift
@@ -2,11 +2,14 @@ public extension Passage {
 
     struct Hooks: Sendable {
         public let account: (any Account)?
+        public let passkey: (any Passkey)?
 
         public init(
             account: (any Account)? = nil,
+            passkey: (any Passkey)? = nil,
         ) {
             self.account = account
+            self.passkey = passkey
         }
     }
 
